2875952f6b3e686b7b7df5e7596c2e15e825eb7d
Luis Gerardo <luisgpr99@gmail.com>
2020-12-03 14:36:26 -0600

    Add basic structure of the project

---
+++ src/Pages/Hunter/Hunter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HunterPage() {
+  return <div>Hunter!</div>;
+}
+
+export default HunterPage;
---
+++ src/Pages/Leader/Leader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LeaderPage() {
+  return <div>Leader!</div>;
+}
+
+export default LeaderPage;
