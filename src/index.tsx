dff4daffe4e3899365278c74d0e773f45456023c
Luis Gerardo <luisgpr99@gmail.com>
2020-12-03 22:40:24 -0600

    Add routing to web page with authorization

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import LoginPage from "./Pages/Login/Login";
+import RegisterPage from "./Pages/Register/Register";
+import LeaderPage from "./Pages/Leader/Leader";
+import HunterPage from "./Pages/Hunter/Hunter";
+import ProtectedRoute from "./Components/ProtectedRoute";
+
 import "./index.css";
-import App from "./App";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={hist}>
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+        <Route path='/login' exact component={LoginPage} />
+        <Route path='/register' component={RegisterPage} />
+        <ProtectedRoute
+          path='/leaders'
+          component={LeaderPage}
+          isAuthenticated={false}
+        />
+        <ProtectedRoute
+          path='/hunters'
+          component={HunterPage}
+          isAuthenticated={false}
+        />
+        <Redirect from='*' to='/' />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
