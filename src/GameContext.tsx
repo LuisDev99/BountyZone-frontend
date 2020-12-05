345d7861302488e64cb11f83e96d3b32fcd0a7a4
Luis Gerardo <luisgpr99@gmail.com>
2020-12-05 02:06:23 -0600

    Add resetContext function to the Game Context.  This function allows resetting all the context state back to undefined, to avoid side effects when user session ends.
    Also remove unnecessary optional functions in the GameContext interface.

--- src/GameContext.tsx
+++ src/GameContext.tsx
@@ -7,12 +7,18 @@ export interface GameContext {
   player?: Player;
   leader?: Leader;
   hunter?: Hunter;
-  setPlayerContext?: (player: Player) => void;
-  setLeaderContext?: (leader: Leader) => void;
-  setHunterContext?: (hunter: Hunter) => void;
+  resetContext: () => void;
+  setPlayerContext: (player: Player) => void;
+  setLeaderContext: (leader: Leader) => void;
+  setHunterContext: (hunter: Hunter) => void;
 }
 
-const GameAppContext = React.createContext<GameContext>({});
+const GameAppContext = React.createContext<GameContext>({
+  resetContext: () => {},
+  setPlayerContext: (player: Player) => {},
+  setLeaderContext: (leader: Leader) => {},
+  setHunterContext: (hunter: Hunter) => {},
+});
 
 export function useGameContext() {
   return useContext(GameAppContext);
@@ -23,12 +29,19 @@ export function GameProvider({ children }: any) {
   const [leader, setLeaderContext] = useState<Leader | undefined>(undefined);
   const [hunter, setHunterContext] = useState<Hunter | undefined>(undefined);
 
+  function resetContext() {
+    setPlayerContext(undefined);
+    setLeaderContext(undefined);
+    setHunterContext(undefined);
+  }
+
   return (
     <GameAppContext.Provider
       value={{
         hunter,
         leader,
         player,
+        resetContext,
         setPlayerContext,
         setHunterContext,
         setLeaderContext,
