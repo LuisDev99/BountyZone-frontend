d396c7fba6cd902d02224d925bedc1a774c707b7
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 19:51:59 -0600

    Add GetRouteBasedOnRole helper function to Constants.ts

--- src/Helpers/Constants.ts
+++ src/Helpers/Constants.ts
@@ -1,3 +1,5 @@
+import { PlayerRole } from "../Models/Player";
+
 const Constants = {
   DEV_API_URL: "https://localhost:44369/api/",
   PROD_API_URL: "",
@@ -8,6 +10,20 @@ export function GetAPIURL() {
     return Constants.DEV_API_URL;
 
   return Constants.PROD_API_URL;
+}
+
+export function GetRouteBasedOnRole(role: PlayerRole): string {
+  if (role.Type === "Leader")
+    return "/leaders";
+
+  if (role.Type === "Hunter")
+    return "/hunters";
+
+  return "/";
+}
+
+export const CSS_THEME = {
+  color: "#0ef052"
 }
 
 export default Constants;
