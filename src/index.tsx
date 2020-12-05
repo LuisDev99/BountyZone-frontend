5bc87fd29a68c54cb0cb7bb4f5d6927d4dfa7b71
Luis Gerardo <luisgpr99@gmail.com>
2020-12-04 19:53:34 -0600

    Add Css theme for the style components

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,8 @@ import ProtectedRoute from "./Components/ProtectedRoute";
 import "./index.css";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import CreateRole from "./Pages/CreateRole/CreateRole";
+import { ThemeProvider } from "styled-components";
+import { CSS_THEME } from "./Helpers/Constants";
 
 const hist = createBrowserHistory();
 const uri = process.env.REACT_APP_AUTH0_CALLBACK_URL ?? "";
@@ -20,22 +22,20 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? "";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={uri}>
-      <Router history={hist}>
-        <Switch>
-          <Route path='/' exact component={LandingPage} />
-          <Route path='/login' exact component={LoginPage} />
-          <Route path='/register' component={CreateRole} />
-          <ProtectedRoute
-            path='/leaders'
-            component={LeaderPage}
-            isAuthenticated={false}
-          />
-          <ProtectedRoute path='/hunters' component={HunterPage} />
-          <Redirect from='*' to='/' />
-        </Switch>
-      </Router>
-    </Auth0Provider>
+    <ThemeProvider theme={CSS_THEME}>
+      <Auth0Provider domain={domain} clientId={clientId} redirectUri={uri}>
+        <Router history={hist}>
+          <Switch>
+            <Route path='/' exact component={LandingPage} />
+            <Route path='/login' exact component={LoginPage} />
+            <ProtectedRoute path='/register' component={CreateRole} />
+            <ProtectedRoute path='/leaders' component={LeaderPage} />
+            <ProtectedRoute path='/hunters' component={HunterPage} />
+            <Redirect from='*' to='/' />
+          </Switch>
+        </Router>
+      </Auth0Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
