9337a1abb961f9f09eb4907c6f69c01704ff8ed0
Luis Gerardo <luisgpr99@gmail.com>
2020-12-05 00:24:39 -0600

    Add Leader interface model

---
+++ src/Models/Leader.ts
@@ -0,0 +1,10 @@
+import Player from "./Player";
+
+export interface Leader {
+  Money: number;
+  Reputation: number;
+  SuccessfulAttacks: number;
+  SuccessfulDefends: number;
+  PlayerID: number;
+  Player: Player;
+}
