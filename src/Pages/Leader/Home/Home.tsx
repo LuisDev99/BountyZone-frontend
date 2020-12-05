75f57d3c86b3b43412e0e91da9027cc3d91e3c19
Luis Gerardo <luisgpr99@gmail.com>
2020-12-05 02:36:43 -0600

    Initial design for the Home of the Leader player

--- src/Pages/Leader/Home/Home.tsx
+++ src/Pages/Leader/Home/Home.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import useAsyncEffect from "use-async-effect";
+import { useGameContext } from "../../../GameContext";
+import PlayerService from "../../../Services/LeaderService";
+import { LeaderProfileCard } from "./LeaderProfileCard";
 
 export default function Home() {
-  return <div>CASA!!!!</div>;
+  const { player, leader, setLeaderContext } = useGameContext();
+
+  useAsyncEffect(async () => {
+    // If component did mount and there is no leader in the context,
+    // fetch the leader by the player ID
+    if (!leader) {
+      try {
+        const response = await new PlayerService().getLeaderByPlayerID(
+          player!.ID
+        );
+
+        setLeaderContext(response.data);
+      } catch (e) {
+        console.log("Error while loading the hunter by its player ID.");
+        console.log({ e });
+      }
+    }
+  }, []);
+
+  if (!leader) {
+    return <h3>Loading your profile.... You will be out there soon</h3>;
+  }
+
+  return (
+    <div>
+      <LeaderProfileCard leader={leader} />
+    </div>
+  );
 }
