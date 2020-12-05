3ee5e218c3b96684f82fa4bdaf77c3d94630aaa5
Luis Gerardo <luisgpr99@gmail.com>
2020-12-05 02:07:58 -0600

    ProtectedRoute.tsx now resets the whole context if the user is not authenticated.
    This is to ensure that there will be no side effects if the user session ends

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Route, Redirect } from "react-router-dom";
+import { useGameContext } from "../GameContext";
 
 const ProtectedRoute = ({ component: Component, ...rest }: any) => {
   const { isAuthenticated } = useAuth0();
+  const { resetContext } = useGameContext();
 
   if (!isAuthenticated) {
+    resetContext();
+
     return (
       <Redirect
         to={{
