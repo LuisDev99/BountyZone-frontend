95a864ea918ba4940e0f53a481e6711200af385a
Luis Gerardo <luisgpr99@gmail.com>
2020-12-06 00:06:46 -0600

    Refactor Leader's Home component to use the new custom useLeaderInfo hook

--- src/Pages/Leader/Home/Home.tsx
+++ src/Pages/Leader/Home/Home.tsx
@@ -1,29 +1,10 @@
 import React from "react";
-import useAsyncEffect from "use-async-effect";
-import { useGameContext } from "../../../GameContext";
-import PlayerService from "../../../Services/PlayerService";
+import { useLeaderInfo } from "../../../Helpers/CharactersHooks";
 import { LeaderProfileCard } from "./LeaderProfileCard";
 import PopularVictims from "./PopularVictims";
 
 export default function Home() {
-  const { player, leader, setLeaderContext } = useGameContext();
-
-  useAsyncEffect(async () => {
-    // If component did mount and there is no leader in the context,
-    // fetch the leader by the player ID
-    if (!leader) {
-      try {
-        const response = await new PlayerService().getLeaderByPlayerID(
-          player!.ID
-        );
-
-        setLeaderContext(response.data);
-      } catch (e) {
-        console.log("Error while loading the hunter by its player ID.");
-        console.log({ e });
-      }
-    }
-  }, []);
+  const { leader } = useLeaderInfo();
 
   if (!leader) {
     return <h3>Loading your profile.... You will be out there soon</h3>;
