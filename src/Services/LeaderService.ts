45d2d4bfed7557b323c2f770cd8f78cd0534d4ad
Luis Gerardo <luisgpr99@gmail.com>
2020-12-05 19:19:55 -0600

    Add placeBountyOnVictim method to the LeaderService class

--- src/Services/LeaderService.ts
+++ src/Services/LeaderService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GetAPIURL } from "../Helpers/Constants";
+import { Bounty } from "../Models/Bounty";
 import { Leader } from "../Models/Leader";
 
 class LeaderService {
@@ -7,6 +8,12 @@ class LeaderService {
 
   constructor() {
     this.URL = GetAPIURL() + "leaders";
+  }
+
+  placeBountyOnVictim(bounty: Bounty) {
+    return axios.post(`${this.URL}/place-bounty`, {
+      ...bounty
+    })
   }
 
   getPopularVictims() {
