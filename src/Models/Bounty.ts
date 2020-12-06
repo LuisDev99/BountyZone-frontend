693d1a3284b89677ef0fd44e3393d2248e941c12
Luis Gerardo <luisgpr99@gmail.com>
2020-12-06 15:45:13 -0600

    Refactor Bounty model interface. Only the Hunter and HunterID properties should be undefined.
    Add new AddBounty model that will be used when the leader creates a new bounty

--- src/Models/Bounty.ts
+++ src/Models/Bounty.ts
@@ -2,16 +2,24 @@ import { Hunter } from "./Hunter";
 import { Leader } from "./Leader";
 
 export interface Bounty {
+  ID: number;
   Time: Date;
   Price: number;
-  Bribed?: boolean;
-  IsConfirmed?: boolean;
-  Leader?: Leader;
-  Victim?: Leader;
+  Bribed: boolean;
+  IsConfirmed: boolean;
+  Leader: Leader;
+  Victim: Leader;
   Hunter?: Hunter;
   LeaderID: number;
   VictimID: number;
   HunterID?: number;
+}
+
+export interface AddBounty {
+  Price: number;
+  LeaderID: number;
+  Time: Date;
+  VictimID: number;
 }
 
 export interface BountyInfo {
