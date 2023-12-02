ab01298dafe033b4224571a5ae0fed59c1b82dc7
Luis Ponce <luisgpr99@gmail.com>
2023-12-02 17:30:22 -0600

    Update variables

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 - Context API
 - Styled Components
 - Typescript
+
+### Runtime
+
+- Node version 16
+- Environment variables:
+
+```
+REACT_APP_AUTH0_CALLBACK_URL=<base-url>/login
+REACT_APP_AUTH0_DOMAIN=<domain>
+REACT_APP_AUTH0_CLIENT_ID=<client-id>
+REACT_APP_DEV_URL=https://localhost:5000/api/
+REACT_APP_PROD_URL=<prod-url>
+```
--- src/Helpers/Constants.ts
+++ src/Helpers/Constants.ts
@@ -1,29 +1,26 @@
 import { PlayerRole } from "../Models/Player";
 
 const Constants = {
-  DEV_API_URL: "https://localhost:44369/api/",
-  PROD_API_URL: "https://bountyzoneapi20201207013622.azurewebsites.net/api/",
-}
+  DEV_API_URL: process.env.REACT_APP_DEV_URL,
+  PROD_API_URL: process.env.REACT_APP_PROD_URL,
+};
 
 export function GetAPIURL() {
-  if (process.env.NODE_ENV === 'development')
-    return Constants.DEV_API_URL;
+  if (process.env.NODE_ENV === "development") return Constants.DEV_API_URL;
 
   return Constants.PROD_API_URL;
 }
 
 export function GetRouteBasedOnRole(role: PlayerRole): string {
-  if (role.Type === "Leader")
-    return "/leaders";
+  if (role.Type === "Leader") return "/leaders";
 
-  if (role.Type === "Hunter")
-    return "/hunters";
+  if (role.Type === "Hunter") return "/hunters";
 
   return "/";
 }
 
 export const CSS_THEME = {
-  color: "#0ef052"
-}
+  color: "#0ef052",
+};
 
 export default Constants;
